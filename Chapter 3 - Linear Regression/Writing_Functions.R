eb4bfb66b4f83292677eea1e00b150d6962bff60
ishaanCubyts <ishaan@cubyts.com>
2022-09-15 21:41:50 -0700

    Update Writing_Functions.R

---
+++ Chapter 3 - Linear Regression/Writing_Functions.R
@@ -0,0 +1,9 @@
+#Creating your own function to load certain libraries
+
+LoadLibraries <- function(){
+  library(ISLR2)
+  library(MASS)
+  print("The libraries have now been loaded")
+}
+
+LoadLibraries()
